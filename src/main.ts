ec76c2ce52bd2e653e8a561242d6278ca24feebd
sarah kiarie <sarahwanjiruki1@gmail.com>
2025-08-19 11:43:54 +0300

    fixed cors issue

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
